b6e23ede56a212c5ade702dfa4a24d3e449d8ad3
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-07-06 02:15:22 +0300

    simpleArray

---
+++ phpMultiply/phpArrays.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<?php
+$arr = [10,20,30,40,50];
+echo implode(",",$arr);
+?>
+</body>
+</html>
